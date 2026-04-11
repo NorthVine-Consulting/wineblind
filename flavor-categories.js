e896f4c6e15df43b3dd88cc8645ba19688cb8787
keighty <k80.leonard@gmail.com>
2026-04-11 13:28:03 -0700

    Extract flavor categories

---
+++ flavor-categories.js
@@ -0,0 +1,47 @@
+const FLAVOR_CATEGORIES = {
+  'Fruit': [
+    'lemon', 'lime', 'grapefruit', 'mandarin',
+    'peach', 'apricot', 'white peach',
+    'tropical fruit', 'passion fruit', 'mango', 'lychee',
+    'green apple', 'pear', 'quince',
+    'cherry', 'red cherry', 'sour cherry', 'black cherry', 'raspberry',
+    'strawberry', 'cranberry', 'red plum', 'plum', 'dark cherry',
+    'blackberry', 'blackcurrant', 'cassis', 'blueberry',
+    'gooseberry', 'grape', 'banana', 'orange peel', 'kirsch',
+    'blackberry jam', 'dried fruit', 'dried apricot', 'dried fig',
+    'prune', 'fig', 'fruitcake', 'dark fruit', 'tangerine'
+  ],
+  'Floral': [
+    'rose', 'rose petal', 'dried rose', 'violet', 'violets', 'jasmine',
+    'white flowers', 'blossom', 'orange blossom', 'honeysuckle',
+    'elderflower', 'dried flowers'
+  ],
+  'Spice': [
+    'black pepper', 'white pepper',
+    'ginger', 'clove', 'anise', 'dill', 'baked spice', 'jalapeño'
+  ],
+  'Earth/Mineral': [
+    'earth', 'earthy', 'mushroom', 'forest floor', 'sous bois',
+    'graphite', 'slate', 'petrol', 'flint', 'iron',
+    'truffle', 'tar', 'crushed gravel', 'petrichor'
+  ],
+  'Nutty/Oak': [
+    'vanilla', 'toast', 'brioche', 'hazelnut', 'almond',
+    'butter', 'cream', 'mocha', 'coffee', 'chocolate',
+    'dark chocolate', 'cedar', 'cigar box', 'smoke',
+    'pencil shavings'
+  ],
+  'Herbal/Vegetal': [
+    'cut grass', 'herbs', 'fresh herbs', 'dried herbs',
+    'nettle', 'bay leaf', 'mint', 'eucalyptus',
+    'thyme', 'garrigue', 'tomato leaf', 'leafy', 'tobacco',
+    'green pepper', 'red pepper', 'dill'
+  ],
+  'Other': [
+    'honey', 'beeswax', 'turkish delight', 'musk', 'wet wool', 'lanolin',
+    'game', 'smoked meat', 'bacon', 'leather', 'olive', 'cola',
+    'bubblegum', 'balsamic', 'cinnamon gum'
+  ]
+};
+
+const CATEGORY_ORDER = ['Fruit', 'Floral', 'Spice', 'Earth/Mineral', 'Herbal/Vegetal', 'Nutty/Oak', 'Other'];
